8b8a485839b262c372bdc48703329d813d26134c
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-12 14:49:44 +0200

    Create Controllers Create AuthorsController for routing

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Library.API.Controllers
-{
+{   [Route("api/authors")]
     public class AuthorsController :Controller
     {
         private ILibraryRepository _libraryRepository;
@@ -16,11 +16,20 @@ namespace Library.API.Controllers
             _libraryRepository = libraryRepository;
         }
 
+        [HttpGet()]
         public IActionResult GetAuthors() //IActionResult defines a contract that represents the result of an action method
         {
             var authorsFromRepo = _libraryRepository.GetAuthors();
             //Serialize the result as JSON
             return new JsonResult(authorsFromRepo); // JsonResult returns the given object as JSON
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(Guid id) //IActionResult defines a contract that represents the result of an action method
+        {
+            var authorFromRepo = _libraryRepository.GetAuthor(id);
+            //Serialize the result as JSON
+            return new JsonResult(authorFromRepo); // JsonResult returns the given object as JSON
         }
     }
 }
