a5c87b105eb8362e499540699a475418c8f484dd
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-12 18:14:42 +0200

    Add Child/Parent Relationships Create BookDTO Map BookDTO to Book Entity through auto mapper Create Book Controller Get books for an author Get book for an author

---
+++ src/Library.API/Controllers/BooksController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Library.API.Model;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors/{authorId}/books")]
+    public class BooksController:Controller
+    {
+        private ILibraryRepository _libraryRepository;
+
+        public BooksController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet()]
+        public IActionResult GetBooksForAuthor(Guid authorId)
+        {
+            var booksForAuthorFromRepo = _libraryRepository.GetBooksForAuthor(authorId);
+
+            if (!_libraryRepository.AuthorExists(authorId))
+                return NotFound();
+
+            var books = Mapper.Map<IEnumerable<BookDTO>>(booksForAuthorFromRepo);
+            return Ok(books);
+        }
+
+        [HttpGet("{bookId}")]
+        public IActionResult GetBookForAuthor(Guid authorId,Guid bookId)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+                return NotFound();
+
+            var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId,bookId);
+
+            if (bookForAuthorFromRepo == null)
+                return NotFound();
+            var book = Mapper.Map<BookDTO>(bookForAuthorFromRepo);
+
+            return Ok(book);
+        }
+    }
+}
--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -70,6 +70,7 @@ namespace Library.API
                      $"{src.FirstName} {src.LastName}"))
                     .ForMember(dest => dest.Age, opt => opt.MapFrom(src =>
                      src.DateOfBirth.GetCurrentAge()));
+                cfg.CreateMap<Entities.Book, Model.BookDTO>();
             });
 
             libraryContext.EnsureSeedDataForContext();
